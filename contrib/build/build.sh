0dee2ef3f163a02579e27815dc230039055ae07d
James Swineson <github@public.swineson.me>
2021-10-19 00:39:51 +0800

    add debian deps

--- contrib/build/build.sh
+++ contrib/build/build.sh
@@ -5,19 +5,19 @@ cd "$( dirname "${BASH_SOURCE[0]}" )/../.."
 
 # set GOROOT to the actual goroot, else you will have strange errors complaining cannot load bufio
 # fix GOPATH if it doesn't exist
-export GOPATH=${GOPATH:-/tmp/gopath}
-OUT_FILE=${OUT_FILE:-serial}
+export GOPATH="${GOPATH:-/tmp/gopath}"
+OUT_FILE="${OUT_FILE:-serial}"
 
 export CGO_CPPFLAGS="${CPPFLAGS:-}"
 export CGO_CFLAGS="${CFLAGS:-}"
 export CGO_CXXFLAGS="${CXXFLAGS:-}"
 export CGO_LDFLAGS="${LDFLAGS:-}"
 
-GIT_COMMIT=$(git rev-list -1 HEAD | cut -c -8)
-CURRENT_TIME=$(date -u "+%Y-%m-%d %T UTC")
-COMPILE_HOST=$(cat /proc/sys/kernel/hostname)
+GIT_COMMIT="$(git rev-list -1 HEAD | cut -c -8)"
+CURRENT_TIME="$(date -u "+%Y-%m-%d %T UTC")"
+COMPILE_HOST="$(cat /proc/sys/kernel/hostname)"
 GIT_STATUS=""
-if output=$(git status --porcelain) && [ -z "$output" ]; then
+if output="$(git status --porcelain)" && [ -z "$output" ]; then
 	GIT_STATUS="clean"
 else
 	GIT_STATUS="dirty"
